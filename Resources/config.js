c3aa08eb619684428d2811a2c8cbe81bae0f3974
motoy3d <motoy3d@gmail.com>
2014-12-15 04:10:11 +0900

    first commit

---
+++ Resources/config.js
@@ -0,0 +1,27 @@
+exports.config = {
+    teamId: 'grampus'
+    ,hashtag: 'grampus'
+    ,teamName: '名古屋'
+    ,appName: 'スマートJ for 名古屋グランパス'
+    ,iPhoneAppUrl: 'https://itunes.apple.com/jp/app/id920460662?l=ja&ls=1&mt=8'
+    ,androidAppUrl: 'https://play.google.com/store/apps/details?id=smartj.grampus'
+    ,color: '#ff0000'
+    ,tintColor: '#ffaf00'
+    ,dbName: 'smartj.grampus'
+    //ニュースのソースURL
+    ,feedUrlBase: 'http://sub0000499082.hmk-temp.com/redsmylife/news.json'
+    //日程・結果のソースURL
+    ,resultsUrl: "http://sub0000499082.hmk-temp.com/redsmylife/results.json?teamId=grampus&season="
+    //設定、メッセージのソースURL
+    ,messageUrl: "http://sub0000499082.hmk-temp.com/redsmylife/config.json?teamId=grampus"
+    ,newsEntriesPerPage: 50
+    ,googleAnalyticsTrackingId: 'UA-30928840-1'
+    //nend(iPhone)
+    ,nendSpotIdIPhoneIcon: 236272
+    ,nendApiKeyIPhoneIcon: 'e9dab0141da35e172335fe63e78327071893348d'
+    ,nendSpotIdIPhoneBanner: 236271
+    ,nendApiKeyIPhoneBanner: '593b5e565625251d325b0f30399762518ee1c0f3'
+    //nend(Android)
+    ,nendSpotIdAndroid: 236273
+    ,nendApiKeyAndroid: 'cc6fb84687cfa762a979fbc1711eedbf12f6bbec'
+};
